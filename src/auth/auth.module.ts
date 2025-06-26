77a50626daf47fcdb1e3ae5c531afa8945a23cd6
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-06-26 14:32:51 -0300

    feat(auth): export JwtModule from AuthModule
    - Updated `AuthModule` to include `JwtModule` in its exports.
    - Enables external modules to reuse JWT functionality directly from `AuthModule`.
    
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -20,6 +20,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
